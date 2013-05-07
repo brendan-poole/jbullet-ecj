f7a95325616c21f90309eccf0946b26f05f77552
brendan.poole <brendan.poole@gmail.com>
2013-05-07 21:44:59 +0100

    mvc - view working

--- src/physicsevolve/Controller.java
+++ src/physicsevolve/Controller.java
@@ -11,25 +11,26 @@ public class Controller {
 	public static void main(String[] args) {
 		Model model1 = new Model();
 		Model model2 = new Model();
-		//View view = new View(model);
+		View view = new View(model1);
 		
         int frame = 0;
 		Vector3f v1 = new Vector3f();
 		Vector3f v2 = new Vector3f();
-        while (frame < 1000  /* && !view.isCloseRequested() */) {
-    		//while(clock.getTimeMicroseconds()<1000000f/60f) {  }
+        while (frame < 1000  && !view.isCloseRequested()) {
+    		while(clock.getTimeMicroseconds()<1000000f/60f) {  }
+    		clock.reset();
 			model1.bodies.get("box").getCenterOfMassPosition(v1);
 			model2.bodies.get("box").getCenterOfMassPosition(v2);
 			System.out.println(v1.y +" "+v2.y);
             model1.move();
             model2.move();
-            //view.render();
+            view.render();
             frame++;
         }
         model1.dynamicsWorld.destroy();
         model1.dynamicsWorld = null;
         model2.dynamicsWorld.destroy();
         model2.dynamicsWorld = null;
-        //view.destroy();
+        view.destroy();
 	}
 }
--- src/physicsevolve/View.java
+++ src/physicsevolve/View.java
@@ -1,6 +1,19 @@
 package physicsevolve;
 
-import static com.bulletphysics.demos.opengl.IGL.*;
+import static com.bulletphysics.demos.opengl.IGL.GL_AMBIENT;
+import static com.bulletphysics.demos.opengl.IGL.GL_COLOR_BUFFER_BIT;
+import static com.bulletphysics.demos.opengl.IGL.GL_DEPTH_BUFFER_BIT;
+import static com.bulletphysics.demos.opengl.IGL.GL_DEPTH_TEST;
+import static com.bulletphysics.demos.opengl.IGL.GL_DIFFUSE;
+import static com.bulletphysics.demos.opengl.IGL.GL_LESS;
+import static com.bulletphysics.demos.opengl.IGL.GL_LIGHT0;
+import static com.bulletphysics.demos.opengl.IGL.GL_LIGHT1;
+import static com.bulletphysics.demos.opengl.IGL.GL_LIGHTING;
+import static com.bulletphysics.demos.opengl.IGL.GL_MODELVIEW;
+import static com.bulletphysics.demos.opengl.IGL.GL_POSITION;
+import static com.bulletphysics.demos.opengl.IGL.GL_PROJECTION;
+import static com.bulletphysics.demos.opengl.IGL.GL_SMOOTH;
+import static com.bulletphysics.demos.opengl.IGL.GL_SPECULAR;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,8 +30,14 @@ import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.PixelFormat;
 
 import com.bulletphysics.BulletGlobals;
+import com.bulletphysics.collision.dispatch.CollisionObject;
+import com.bulletphysics.demos.opengl.GLShapeDrawer;
 import com.bulletphysics.demos.opengl.IGL;
+import com.bulletphysics.demos.opengl.LWJGL;
+import com.bulletphysics.dynamics.RigidBody;
+import com.bulletphysics.linearmath.DefaultMotionState;
 import com.bulletphysics.linearmath.QuaternionUtil;
+import com.bulletphysics.linearmath.Transform;
 import com.bulletphysics.linearmath.VectorUtil;
 
 import ec.app.physics.PhysicsProblem;
@@ -34,26 +53,82 @@ public class View {
 	protected final Vector3f cameraTargetPosition = new Vector3f(0f, 0f, 0f);
 	protected final Vector3f cameraUp = new Vector3f(0f, 1f, 0f);
 	protected int forwardAxis = 2;
-	
-	Model m;
+	private Vector3f wireColor = new Vector3f();
+	private final Transform tr = new Transform();
+	Model model;
 
 	public void render() {
 		Display.update();
+		gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		int numObjects = model.dynamicsWorld.getNumCollisionObjects();
+		wireColor.set(1f, 0f, 0f);
+		for (int i = 0; i < numObjects; i++) {
+			CollisionObject colObj = model.dynamicsWorld
+					.getCollisionObjectArray().getQuick(i);
+			RigidBody body = RigidBody.upcast(colObj);
+
+			if (body != null && body.getMotionState() != null) {
+				DefaultMotionState myMotionState = (DefaultMotionState) body
+						.getMotionState();
+				tr.set(myMotionState.graphicsWorldTrans);
+			} else {
+				colObj.getWorldTransform(tr);
+			}
+
+			wireColor.set(1f, 1f, 0.5f); // wants deactivation
+			if ((i & 1) != 0) {
+				wireColor.set(0f, 0f, 1f);
+			}
+
+			// color differently for active, sleeping, wantsdeactivation states
+			if (colObj.getActivationState() == 1) // active
+			{
+				if ((i & 1) != 0) {
+					// wireColor.add(new Vector3f(1f, 0f, 0f));
+					wireColor.x += 1f;
+				} else {
+					// wireColor.add(new Vector3f(0.5f, 0f, 0f));
+					wireColor.x += 0.5f;
+				}
+			}
+			if (colObj.getActivationState() == 2) // ISLAND_SLEEPING
+			{
+				if ((i & 1) != 0) {
+					// wireColor.add(new Vector3f(0f, 1f, 0f));
+					wireColor.y += 1f;
+				} else {
+					// wireColor.add(new Vector3f(0f, 0.5f, 0f));
+					wireColor.y += 0.5f;
+				}
+			}
+
+			GLShapeDrawer.drawOpenGL(gl, tr, colObj.getCollisionShape(),
+					wireColor, 0);
+		}
+
+		// gl.glDisable(GL_LIGHTING);
+		// gl.glColor3f(0f, 0f, 0f);
+		// gl.glEnable(GL_LIGHTING);
+		updateCamera();
 	}
-	
-	public View(Model m) {
-        try {
-            Display.setDisplayMode(new DisplayMode(800, 600));
-            Display.setTitle("Physics");
-            Display.create(new PixelFormat(0, 24, 0));
-            Keyboard.create();
-            Keyboard.enableRepeatEvents(true);
-            Mouse.create();
-        } catch (LWJGLException ex) {
-            Logger.getLogger(PhysicsProblem.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        reshape(800, 600);
-        setCameraDistance(10f);
+
+	public View(Model model) {
+		this.model = model;
+		try {
+			Display.setDisplayMode(new DisplayMode(1024, 768));
+			Display.setTitle("Physics");
+			Display.create(new PixelFormat(0, 24, 0));
+			Keyboard.create();
+			Keyboard.enableRepeatEvents(true);
+			Mouse.create();
+		} catch (LWJGLException ex) {
+			Logger.getLogger(PhysicsProblem.class.getName()).log(Level.SEVERE,
+					null, ex);
+		}
+		gl = LWJGL.getGL();
+		reshape(1024, 768);
+		setCameraDistance(10f);
 
 		float[] light_ambient = new float[] { 0.2f, 0.2f, 0.2f, 1.0f };
 		float[] light_diffuse = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
@@ -82,7 +157,7 @@ public class View {
 
 		gl.glClearColor(0.7f, 0.7f, 0.7f, 0f);
 	}
-	
+
 	public void reshape(int w, int h) {
 		glutScreenWidth = w;
 		glutScreenHeight = h;
@@ -90,10 +165,11 @@ public class View {
 		gl.glViewport(0, 0, w, h);
 		updateCamera();
 	}
+
 	public void setCameraDistance(float dist) {
 		cameraDistance = dist;
 	}
-	
+
 	public void updateCamera() {
 		gl.glMatrixMode(GL_PROJECTION);
 		gl.glLoadIdentity();
@@ -129,23 +205,22 @@ public class View {
 		if (glutScreenWidth > glutScreenHeight) {
 			float aspect = glutScreenWidth / (float) glutScreenHeight;
 			gl.glFrustum(-aspect, aspect, -1.0, 1.0, 1.0, 10000.0);
-		}
-		else {
+		} else {
 			float aspect = glutScreenHeight / (float) glutScreenWidth;
 			gl.glFrustum(-1.0, 1.0, -aspect, aspect, 1.0, 10000.0);
 		}
-		
+
 		gl.glMatrixMode(GL_MODELVIEW);
 		gl.glLoadIdentity();
 		gl.gluLookAt(cameraPosition.x, cameraPosition.y, cameraPosition.z,
-				cameraTargetPosition.x, cameraTargetPosition.y, cameraTargetPosition.z,
-				cameraUp.x, cameraUp.y, cameraUp.z);
+				cameraTargetPosition.x, cameraTargetPosition.y,
+				cameraTargetPosition.z, cameraUp.x, cameraUp.y, cameraUp.z);
 	}
 
 	public void destroy() {
 		Display.destroy();
 	}
-	
+
 	public boolean isCloseRequested() {
 		return Display.isCloseRequested();
 	}
